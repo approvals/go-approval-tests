3e81db5a4a597adc9bc2986297c78259119998c5
Llewellyn Falco <isidore@setgame.com>
2016-07-12 19:13:05 +0300

    added docs

--- utils/testing_utils.go
+++ utils/testing_utils.go
@@ -2,6 +2,8 @@ package utils
 
 import "testing"
 
+// AssertEqual Example:
+// AssertEqual(t, 10, number, "number")
 func AssertEqual(t *testing.T, expected, actual interface{}, message string) {
 	if expected != actual {
 		t.Fatalf(message+"\n[expected != actual]\n[%s != %s]", expected, actual)
