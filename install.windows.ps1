49eff2bc73947f74c61977f2c72b70a362d4ae0b
Llewellyn Falco <isidore@setgame.com>
2022-02-17 12:09:42 -0700

    Update install.windows.ps1

--- install.windows.ps1
+++ install.windows.ps1
@@ -1,5 +1,5 @@
 # How to run this file:
-#  PS> iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/dev_environments/golang.ps1 | iex
+# iwr -useb https://raw.githubusercontent.com/approvals/go-approval-tests/main/install.windows.ps1 | iex
 
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/golang-goland.ps1 | iex
