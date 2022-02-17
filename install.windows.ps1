9d5ecdc204de237e1f54d9f5b5b1e08c04a1cef9
Llewellyn Falco <isidore@setgame.com>
2022-02-17 12:08:34 -0700

    Update install.windows.ps1

--- install.windows.ps1
+++ install.windows.ps1
@@ -4,5 +4,6 @@
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/windows.ps1 | iex
 iwr -useb https://raw.githubusercontent.com/JayBazuzi/machine-setup/main/golang-goland.ps1 | iex
 
-# Clone repo
-& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/OrangePants-R/starter-project-go-coderetreat.git C:\Code\starter-project-go-coderetreat
+
+& "C:\Program Files\Git\cmd\git.exe" clone https://github.com/approvals/go-approval-tests.git C:\Code\go-approval-tests
+cd C:\Code\go-approval-tests
